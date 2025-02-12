9fd3fd4924b27fa546e3e32aa9bcfacbafbdf55a
Nimbus <hossain.mona006@gmail.com>
2025-02-12 16:25:48 +0530

    added stars

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const earth=document.getElementById("earth");
+const body=document.querySelector("body");
+const time=new Date();
+
+//earth properties
 const radius=250;
 const angularVelocity=1;
 const timePeriod=5;
-const frequency=0.5;
-const time=new Date();
+const frequency=0.1;
+
+
 initTime=time.getTime();
 initTime=(initTime/1000);
 console.log(initTime)
+
+
+//stars
+for(let i=0;i<=100;i++){
+    
+    const star=document.createElement("div");
+    //position
+    x=(Math.random()*100).toFixed(0);
+    y=(Math.random()*100).toFixed(0);
+    star.style.top=`${x}%`;
+    star.style.left=`${y}%`;
+    //size
+    let w=(Math.random()/3).toFixed(3);
+    star.style.width=`${w}vw`;
+    star.style.height=`${w}vw`;
+    //colors
+    let h=(Math.random()*361).toFixed(0);
+    let s=(Math.random()*101).toFixed(0);
+    let l=(Math.random()*16).toFixed(0)+60;
+    let color=`hsl(${h},${s}%,${l}%)`
+    star.style.backgroundColor=color;
+    //glow
+    star.style.boxShadow=`0px 0px 10px hsl(${h},70%,50%)`
+    
+    star.classList.add("star");
+    body.append(star);
+    console.log(star.style.width)
+}
+
+
 a=setInterval(()=>{
     newTime=new Date();
     currentTime=newTime.getTime();
@@ -18,8 +53,8 @@ a=setInterval(()=>{
     earth.style.transform=`translate(${x}px,${y}px)`;
     // earth.style.top=`${y}px`;
     // earth.style.right=`${x}px`;
-    if(angularVelocity*runTime>=6.28){
-        console.log(runTime.toFixed(2));
-    }
+    // if(angularVelocity*runTime>=6.28){
+    //     console.log(runTime.toFixed(2));
+    // }
 },1);
 
